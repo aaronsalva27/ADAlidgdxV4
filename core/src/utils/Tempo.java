4bb7050598fea2e5edabecab5de46698916246ab
davrami <david.rami007@gmail.com>
2017-02-12 12:06:01 +0100

    Class Tempo up

---
+++ core/src/utils/Tempo.java
@@ -0,0 +1,38 @@
+package utils;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Tempo {
+    public float Temp = 0;
+
+    public Tempo() {
+    }
+
+    public void setTemp(float temp) {
+        Temp = temp;
+    }
+
+    public float getTemp() {
+        return Temp;
+    }
+
+    public String updateX( float delta){
+        float x = getTemp();
+        x = x + delta;
+        setTemp(x);
+        return String.valueOf(x);
+    }
+
+    public void temporizador(float delta){
+        SpriteBatch spriteBatch = new SpriteBatch();
+        BitmapFont font = new BitmapFont();
+        spriteBatch.begin();
+        font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+        font.draw(spriteBatch, String.valueOf(updateX(delta)) , 680, 450);
+        spriteBatch.end();
+
+    }
+
+
+}
